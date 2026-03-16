94be9ce4116b9e8aac948838b205a4c89137ddc8
ebcengiz <enesbugracengiz@gmail.com>
2026-03-16 04:03:31 +0300

    supabase değişiklikler.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,9 +4,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Supabase yapılandırması
 // NOT: Bu bilgileri kendi Supabase proje bilgilerinizle değiştirin
-const SUPABASE_URL = "https://tjffmvmwncdabgzjtyeb.supabase.co"; // Örnek: https://xxxxx.supabase.co
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZmZtdm13bmNkYWJnemp0eWViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNzUwMjMsImV4cCI6MjA3NDc1MTAyM30.6jQBKlLodGK1LMh2EzKBrZHL2VqlkhC4JYTy0CfdXys";
+const SUPABASE_URL = "https://qyfagnhmhovhlpbllioq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5ZmFnbmhtaG92aGxwYmxsaW9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MTA5NzksImV4cCI6MjA4OTE4Njk3OX0.pa7V7a3aHN11fXeHQZBFLokuJUo_0n4fuCBvP7QF-0A";
 
 // Supabase client oluştur
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
